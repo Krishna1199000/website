20442f1921166be2db8ba47b35e86607cb067c69
krishna <gohilkrishna9004@gmail.com>
2024-10-14 16:41:41 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://lighthearted-pie-08b47b.netlify.app', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     
     credentials: true, 
