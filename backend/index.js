32d381c4046cf8463b6e79bd56700a97a0b25866
krishna <gohilkrishna9004@gmail.com>
2024-10-14 14:43:29 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const path = require('path')
 
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: '', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     
     credentials: true, 
