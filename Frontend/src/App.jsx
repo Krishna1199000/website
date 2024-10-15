e9884b03d612c947c4980270bd7d3e3ab5c3d654
krishna <gohilkrishna9004@gmail.com>
2024-10-15 12:55:08 +0530

    solving refresh error

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -36,7 +36,7 @@ const UserPrivateRoute = ({ children }) => {
 function App() {
     return (
         <RecoilRoot>
-            <Router>
+            <Router basename='/app'>
                 <ToastContainer />
                 <Routes>
                    
